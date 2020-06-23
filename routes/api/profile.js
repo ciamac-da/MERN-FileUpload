6b1b4a4554b96a6d3b7bdb81e618a09fa345f193
ciamac <hgzguzg>
2020-06-23 14:07:20 +0200

    Verified by Cia v1.0.2 Routes added, started making profile.js in backend

---
+++ routes/api/profile.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
+const multer = require("multer");
+const path = require("path");
+const url = require("url");
